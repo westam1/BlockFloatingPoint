dc4ca91a50710c31456b70d11c71cd7f74c84629
westam1 <93354999+westam1@users.noreply.github.com>
2024-08-10 11:14:39 -0400

    Setup of basic bones mostly done

--- BlockFloatingPoint/BlockFloatingPoint.cpp
+++ BlockFloatingPoint/BlockFloatingPoint.cpp
@@ -1,20 +1,81 @@
-// BlockFloatingPoint.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+constexpr auto SUCCESS			= 0;
+constexpr auto FAILURE			= 1;
+
+constexpr auto SZ_STR			= 64;
+constexpr auto SZ_BLOCK			= 16;
+
+constexpr auto MAX_GEN			= 10.f;
+
+#define DISPLAY(A,B)			int2str_b(A.i, B); printf("Float %s = %e, %.8x, %s\n", #A, A.f, A.i, str);
+
+typedef union IFUnion {
+	int32_t i;
+	float f;
+} int_float_t;
+typedef enum OpTypes {
+	op_Add,
+	op_Mul,
+	op_Mac,
+} op_t;
+
+static int int2str_b(int32_t In, char Str[SZ_STR]) {
+	int i2 = 0;
+	for (int i = (sizeof(In) * 8) - 1; i >= 0; i--, i2++) {
+		Str[i2] = (In & (1 << i)) ? '1' : '0';
+		if (i2 && (i % 8) == 0) {
+			Str[++i2] = ' ';
+		}
+	}
+	Str[i2] = '\0';
+
+	return SUCCESS;
+}
+static int Run_BlockOp(int_float_t BlockA[SZ_BLOCK], int_float_t BlockB[SZ_BLOCK], op_t Op) {
+	float gen[SZ_BLOCK]{}, truth[SZ_BLOCK]{}, accGen{}, accTruth{};
+
+	// Generate truth values.
+	for (int i = 0; i < SZ_BLOCK; i++) {
+		switch (Op) {
+			case op_Add: truth[i] = BlockA[i].f + BlockB[i].f; break;
+			case op_Mul: truth[i] = BlockA[i].f * BlockB[i].f; break;
+			case op_Mac: accTruth += (BlockA[i].f * BlockB[i].f); break;
+			default: return FAILURE;
+		}
+	}
+
+	// Generate block fp values.
+	for (int i = 0; i < SZ_BLOCK; i++) {
+		
+		switch (Op) {
+			case op_Add: break;
+			case op_Mul: break;
+			case op_Mac: break;
+			default: return FAILURE;
+		}
+
+	}
+
+	return SUCCESS;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+	int_float_t test{};
+	char str[SZ_STR]{};
+	int_float_t bA[SZ_BLOCK]{}, bB[SZ_BLOCK]{}, bC[SZ_BLOCK]{};
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	std::cout << "Block Floating Point Implementation Test!\n";
+	
+	// TODO: Best way to do this is with repeated tests and built up statistics over many, many blocks
+	for (int i = 0; i < SZ_BLOCK; i++) {
+		bA[i].f = rand() / (MAX_GEN);
+		bB[i].f = rand() / (MAX_GEN);
+	}
+
+	Run_BlockOp(bA, bB, op_Add);
+	Run_BlockOp(bA, bB, op_Mul);
+	Run_BlockOp(bA, bB, op_Mac);
+
+	//DISPLAY(bA[i], str);
+}
